49ae27e328b42c9a27c24d20e8543f7dda67880e
siritha rama guddati <sirithaguddati@gmail.com>
2025-07-28 13:21:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ 0367-valid-perfect-square/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        root=int(num**0.5)
+        if root*root==num:
+            return True
+        else:
+            return False   
+        
