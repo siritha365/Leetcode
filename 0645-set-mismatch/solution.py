3621d7db24a2736f76751cd22fdb5e2ec24714ff
siritha rama guddati <sirithaguddati@gmail.com>
2025-08-25 14:54:13 +0000

    Sync LeetCode submission Runtime - 7 ms (86.00%), Memory - 19.7 MB (99.87%)

---
+++ 0645-set-mismatch/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        return [sum(nums)-sum(set(nums)),sum(range(1,n+1))-sum(set(nums))]
+        
