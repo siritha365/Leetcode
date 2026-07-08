4e4074f4ed701d0f8ede36720b8c51b5949ad5cf
siritha rama guddati <sirithaguddati@gmail.com>
2026-07-08 06:25:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (26.26%)

---
+++ 1965-sum-of-digits-in-base-k/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        s=0
+        while n>0:
+            d=n%k
+            s+=d
+            n=n//k
+        return s    
+        
