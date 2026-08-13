85c218500c514ecde396e2b1fd8d0f2172e453a7
siritha rama guddati <sirithaguddati@gmail.com>
2026-08-13 08:19:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (54.01%)

---
+++ 4286-valid-digit-number/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validDigit(self, n: int, x: int) -> bool:
+        str1=list(str(n))
+        if str1[0]!=str(x) and str(x) in str1:
+            return True
+        else:
+            return False    
+
+
+        
