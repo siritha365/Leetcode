00f18655eeaea3b53e93a250bcdb42549f9a0fa5
siritha rama guddati <sirithaguddati@gmail.com>
2026-09-17 16:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.86%) - LeetSync

---
+++ 3221-find-the-peaks/find-the-peaks.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        count=0
+        result=[]
+        for i in range(1,len(mountain)-1):
+            if (mountain[i-1]<mountain[i]) and (mountain[i]>mountain[i+1]):
+                result.append(i)
+        return result        
+        
